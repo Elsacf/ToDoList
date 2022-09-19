34878a506ea2562bc073cebcc0d962a8899a5d2b
Elsa Castillo Franco <elsacastillofranco@Air-de-Elsa.home>
2022-09-20 01:43:12 +0200

    changes updated

---
+++ src/store/task.js
@@ -0,0 +1,54 @@
+import { defineStore } from 'pinia';
+import supabase from '../supabase/index';
+import userStore from './user';
+
+export default defineStore('tasks', {
+  state: () => ({
+    tasks: [],
+  }),
+  actions: {
+    async fetchTasks() {
+      const { data: tasks } = await supabase
+        .from('tasks')
+        .select('*')
+        .order('id', { ascending: false });
+      this.tasks = tasks;
+    },
+    async addTask(title) {
+      const { data: tasks } = await supabase
+        .from('tasks')
+        .insert([
+          {
+            user_id: userStore().user.id,
+            title,
+            is_complete: false,
+          },
+        ]);
+      this.tasks = tasks;
+    },
+    async checkTaskItem(check, id) {
+      const { data: tasks } = await supabase
+        .from('tasks')
+        .update({ is_complete: check })
+        .match({ id });
+
+      this.tasks = tasks;
+    },
+    async deleteTaskItem(id) {
+      const { data: tasks } = await supabase
+        .from('tasks')
+        .delete()
+        .match({ id });
+
+      this.tasks = tasks;
+    },
+    async editTaskItem(title, id) {
+      const { data: tasks } = await supabase
+        .from('tasks')
+        .update(title)
+        .match({ id });
+
+      this.tasks = tasks;
+    },
+  },
+});
